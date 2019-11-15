17c43fcf29c3fa6a223a2a377bba006bde24a653
ZZD <zhangzhendong@126.com>
2019-11-15 14:50:33 +0800

    u

--- readdepth.py
+++ readdepth.py
@@ -182,7 +182,7 @@ def analyzeRD(RDWindows,WindowsN,OccurredWindowsN,NormalizationOnly=False):
                 MixedRDRs[i][j]=(SR-WR)/SampleN+WR
     rdtestfile=open("data/rdtest.txt","w")
     for i in range(OccurredWindowsN):
-        print("22 %d %f"%(i,MixedRDRs[0][int(RefStartPos[RefInd["chr22"]]/100)+i]),file=rdtestfile)
+        print("%s %d %f"%(i,MixedRDRs[0][i]),file=rdtestfile)
     rdtestfile.close()
     exit(0)
     ''' SIGMA(MixedRDRs)=NM, so, ERD=NM/NM=1
