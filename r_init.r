26e929085fceeb6b2c3865928935da7684db2040
ZZD <zhangzhendong@126.com>
2019-12-05 15:15:21 +0800

    update

--- r_init.r
+++
@@ -1,7 +0,0 @@
-if (!requireNamespace("DNAcopy", quietly = TRUE))
-{
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager",repos='http://cran.us.r-project.org')
-BiocManager::install("DNAcopy",update=FALSE)
-}
-
