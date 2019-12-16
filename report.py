2a9913154653e40ace6283a933545a0da0605ea5
ZZD <zhangzhendong@126.com>
2019-12-16 19:29:50 +0800

    fix vcf format

--- report.py
+++ report.py
@@ -15,18 +15,18 @@ def reportVCF(SVs,RefSeq,OutFile,ReportHeader=False):
             fal=False
             Alt+=a
         INFO="SVTYPE=%s,End=%s"%(SV.Type,SV.BreakRight)
-        print("%s %s * %s %s 100 PASS %s GT"%(SV.Chrom,SV.BreakLeft,Ref,Alt,INFO),end="",file=OutFile)
+        print("%s\t%s\t*\t%s\t%s\t100\tPASS\t%s\tGT"%(SV.Chrom,SV.BreakLeft,Ref,Alt,INFO),end="",file=OutFile)
         SI=0
         for i in range(len(g.SampleNames)):
             if SI>=len(SV.Samples) or i!=SV.Samples[SI][0]:
-                print(" 0/0",end="",file=OutFile)
+                print("\t0/0",end="",file=OutFile)
             else:
-                print(" %s/%s"%(SV.Samples[SI][1][0],SV.Samples[SI][1][1]),end="",file=OutFile)
+                print("\t%s/%s"%(SV.Samples[SI][1][0],SV.Samples[SI][1][1]),end="",file=OutFile)
                 SI+=1
         print("\n",end="",file=OutFile)
 
 def reportVCFHeader(OutFile):
     Header=DefaultHeader
     for s in g.SampleNames:
-        Header+=" "+s
+        Header+="\t"+s
     print(Header,file=OutFile)
