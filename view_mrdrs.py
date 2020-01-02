7193352295324b89e241d03d3e9719517ef3ad26
ZZD <zhangzhendong@126.com>
2020-01-02 16:35:50 +0800

    update view_mrdrs

--- view_mrdrs.py
+++ view_mrdrs.py
@@ -18,6 +18,7 @@ for line in ifile:
     if line[0]=="#":
         sl=line[1:].split()
         Chr=sl[0]
+        #print("Reading %s..."%Chr,file=sys.stderr)
         Length=int(sl[1])
         MRDRs[Chr]=[0]*Length
         Ci=0
@@ -26,6 +27,7 @@ for line in ifile:
     Ci+=1
 
 def show(data,WS,WE):
+    #print(data[WS:WE])
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
     ax1.vlines(range(WS,WE),[0]*(WE-WS),data[WS:WE])
@@ -38,6 +40,8 @@ def help():
 -h                  show this help
 show chr start end
   or chr:start-end  show the mrdr data
+set  key=value      change parameters
+     windowsize=100
 q                   quit
 """
     ,file=sys.stderr)
@@ -66,8 +70,15 @@ while 1:
             Border=int(Length*BorderQ)
             Border=max(MinBorder,Border)
             Border=min(MaxBorder,Border)
+            print("Showing windows %s:%s-%s(WindowSize=%s)"%(Chr,Start,End,WindowSize),file=sys.stderr)
             show(MRDRs[Chr],Start-Border,End+Border)
         except:
             help()
+    elif Command[:3]=="set":
+        for S in Command.split()[1:]:
+            k=S.split("=")[0]
+            if k.upper()=="WINDOWSIZE":
+                v=int(S.split("=")[1])
+                WindowSize=v
     else:
         help()
